c8ebf318c347b2e82b71832986a9b556efaa85a0
cal2195 <cmartin7@tcd.ie>
2016-02-10 04:41:46 +0000

    Added running the default hello-world image!

--- swarmpose/__main__.py
+++ swarmpose/__main__.py
@@ -5,8 +5,15 @@ def main():
 	HOST = "51.255.33.85"
 	PORT = "443" #443 redirected to port 4000 on remote server
 
+	#Connect to remote daemon
 	cli = Client(base_url='tcp://' + HOST + ':' + PORT)
-	print cli.info()
+	#print cli.info()
+
+	#Run the hello-world image and print the output
+	container = cli.create_container(image='hello-world:latest')
+	cli.start(container=container.get('Id'))
+	print cli.logs(container=container.get('Id'))
+	
   	pass
 
 
