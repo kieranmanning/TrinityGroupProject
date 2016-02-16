d9c4e2eafa4f39f066d310eb1babc88d6b5ad1d5
Marijan Gradecak <maki@Marijans-MacBook-Pro.local>
2016-02-16 14:44:24 +0000

    changed search for start to return a list of starting points, added a command line argument parser

--- swarmpose/__main__.py
+++ swarmpose/__main__.py
@@ -1,44 +1,50 @@
 """ main code goes here """
 from docker import Client
+import argparse
 import yaml
+
+def clargs():
+	parser = argparse.ArgumentParser(description='a script to start an application on a distributed system')
+	requiredNamed = parser.add_argument_group('required named arguments')
+	#required arguments
+	requiredNamed.add_argument('-c', '--config', required=True, help='config file describing the application')
+	return parser.parse_args()
+
 
 class Swarmpose():
 
-  def __init__(self):
-    self.HOST = "51.255.33.85"
-    self.PORT = "443" #443 redirected to port 4000 on remote server
+	def __init__(self, yamal):
+		self.yamal = yamal
+		self.HOST = "51.255.33.85"
+		self.PORT = "443" #443 redirected to port 4000 on remote server
 
-    #Connect to remote daemon
-    self.cli = Client(base_url='tcp://' + self.HOST + ':' + self.PORT)
-    # print (cli.info())
+		#Connect to remote daemon
+		self.cli = Client(base_url='tcp://' + self.HOST + ':' + self.PORT)
+		# print (cli.info())
 
-    #Run the hello-world image and print the output
-    self.container = self.cli.create_container(image='hello-world:latest')
-    self.cli.start(container=self.container.get('Id'))
+		#Run the hello-world image and print the output
+		self.container = self.cli.create_container(image='hello-world:latest')
+		self.cli.start(container=self.container.get('Id'))
 
-    print (self.cli.logs(container=self.container.get('Id')).decode('UTF-8'))
-    result = self.cli.inspect_container(container=self.container.get('Id'))
+		print (self.cli.logs(container=self.container.get('Id')).decode('UTF-8'))
+		result = self.cli.inspect_container(container=self.container.get('Id'))
 
-    print ("This image ran on " + result['Node']['Addr'])
+		print ("This image ran on " + result['Node']['Addr'])
 
-    self.parseFile('env.yml')
+		self.parseFile('env.yml')
 
+	def parseFile(self, file):
+		with open(file, 'r') as fh:
+			yaml_dict=yaml.load(fh)
+			print (yaml_dict)
+			return yaml_dict
 
-
-  def parseFile(self, file):
-    with open(file, 'r') as fh:
-      yaml_dict=yaml.load(fh)
-      print (yaml_dict)
-
-def searchForStart(dckerNodes):
-	for t in range(dckerNodes):
-		if(dckerNodes[t]["links"] == ""):
-			return dckerNodes[t]
-	else:
-		return 0
+	def searchForStart(self, dockerNodes):
+		return [img for img in dockerNodes if img['links'] == '']
 
 
 
 if __name__ == '__main__':
-  mySwarmpose= Swarmpose()
+	args = clargs()
+	mySwarmpose= Swarmpose(args.config)
 
